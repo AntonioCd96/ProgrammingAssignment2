e74cea5171e42b361d4326dfb512cac77a66d484
AntonioCd96 <antonio.mcd96@gmail.com>
2020-07-13 19:14:44 -0500

    It has been added a code to calculate the inverse of a square matrix. An explample of well-behaved function is added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is an improved version of solve() function, which is highly used to find
+## the inverse of matrix. Its advantage is calculation time is less than solve
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse. 
+makeCacheMatrix<- function(x = matrix()) {
+        I <- NULL
+        set <- function(y) {
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x
+        setS <- function(solve) I <<- solve
+        getS <- function() I
+        list(set = set, get = get,
+             setS = setS,
+             getS = getS)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+##by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getS()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$setS(I)
+        I
 }
+a<-c(1,4,5)
+b<-c(3,4,7)
+c<-c(5,2,5)
+k<-rbind(a,b,c)
+k
+class(k)
+n<-makeCacheMatrix(k)
+n
+s<-cacheSolve(n)
+s
+
+
